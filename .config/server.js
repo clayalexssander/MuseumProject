5f762de8562340cebdcc5c879ee20fddbc8dad6a
clayalexssander <aclayve100@gmail.com>
2026-03-09 18:13:39 -0300

    feat: Implementation of web server, views, routes and css for style

---
+++ .config/server.js
@@ -0,0 +1,15 @@
+import express from 'express';
+const app = express();
+// tod amaquina quando eu subo um servidor nela, ela responde por um servidor loopBack(Localhost)
+//ou seja localHost: 3000
+const port = process.env.port || 3000;
+
+// seta que o motor de views sera o ejs
+app.set('view engine', 'ejs');
+app.use(express.static('./public'));
+
+app.listen(port, () => {
+    console.log("***Servidor WEB rodando na porta ", port)
+});
+
+export default app;
--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # MuseumProject
 Academic project for WEB developtment classes
+
+comandos para execução:
+1. npm install init, nodemon, ejs, express
